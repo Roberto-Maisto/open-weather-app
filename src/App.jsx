956f81e7073d2cee744336811f2934909ff18557
Roberto <maisto_roberto@hotmail.it>
2022-02-17 16:49:53 +0100

    remover example fixture json adds coordinates display to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import axios from "axios";
 const App = () => {
   const [temp, setTemp] = useState([]);
   const [city, setCity] = useState("");
+  const [coords, setCoords] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       let { latitude, longitude } = position.coords;
+      setCoords(position.coords)
       const weatherResponse = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=ae59e318fd0b4392d31ede47cf68de90`
       );
@@ -23,6 +25,7 @@ const App = () => {
     <div data-cy="weather-display">
       <h4 data-cy="temp">Temperature: {temp}°C </h4>
       <h4 data-cy="city">City: {city} </h4>
+      <p data-cy="coords">You are at latitude {coords.latitude} and longitude {coords.longitude}</p>
     </div>
   );
 };
