43950bfee737134cf3778851f10dd0fdabd9404e
Roberto <maisto_roberto@hotmail.it>
2022-02-17 16:40:49 +0100

    adds cypress intercepts

--- cypress/integration/userCanSeeWeatherAtLocation.feature.js
+++ cypress/integration/userCanSeeWeatherAtLocation.feature.js
@@ -1,11 +1,31 @@
 describe("Location based weather", () => {
-    beforeEach(() => {
-      cy.visit("/");
+  beforeEach(() => {
+    cy.intercept("https://api.openweathermap.org/data/2.5/weather?**", {
+      fixture: "weatherResponse.json",
     });
-    it("is expected to be displayed on initial render", () => {
-      cy.get("[data-cy=weather-display]").within(() => {
-        cy.get("[data-cy=temp]").should("contain", "26°C");
-        cy.get("[data-cy=city]").should("contain", "Viterbo");
-      });
+    cy.intercept("https://api.opencagedata.com/geocode/v1/json?q=**", {
+      fixture: "locationResponse.json",
+    });
+    cy.visit("/", {
+      onBeforeLoad(window) {
+        const stubLocation = {
+          coords: {
+            latitude: 42.420677,
+            longitude: 12.107669,
+          },
+        };
+        cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
+          (callback) => {
+            return callback(stubLocation);
+          }
+        );
+      },
     });
   });
+  it("is expected to be displayed on initial render", () => {
+    cy.get("[data-cy=weather-display]").within(() => {
+      cy.get("[data-cy=temp]").should("contain", "26°C");
+      cy.get("[data-cy=city]").should("contain", "Viterbo");
+    });
+  });
+});
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const App = () => {
   const [temp, setTemp] = useState([]);
   const [city, setCity] = useState("");
-  
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position) => {
       let { latitude, longitude } = position.coords;
@@ -17,7 +17,6 @@ const App = () => {
       );
       setCity(locationResponse.data.results[0].components.city);
     });
-    
   }, []);
 
   return (
